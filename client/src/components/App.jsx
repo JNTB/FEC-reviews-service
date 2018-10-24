25e17b4498cb20b145c22534c97f8a2c17695509
Arash <erani.arash@gmail.com>
2018-10-23 19:38:52 -0700

    created react skeleton

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import axios from "axios";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    //function bindings
+  }
+  render() {
+    return <div />;
+  }
+}
