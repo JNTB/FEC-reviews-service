2c511a234017fd110b9dd27e8a1f27c7decdb86d
Arash <erani.arash@gmail.com>
2018-11-10 17:25:22 -0800

    pls

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,11 +3,13 @@ import ReviewStars from "./ReviewStars";
 import Miniheader from "./Miniheader";
 import Pagination from "./Pagination";
 import styles from "../css/app.css";
+import axios from "axios";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
+    axios.defaults.baseURL = "http://" + "localhost:2000";
   }
 
   render() {
